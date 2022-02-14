23420daa2c6702d2335116cc9719d940096830f8
Datzz <74682589+ofDataa@users.noreply.github.com>
2022-02-14 22:52:19 +0200

    Create BoneIDS.cpp

---
+++ fortnite/BoneIDS.cpp
@@ -0,0 +1,16 @@
+// cpp
+Vector3 vHeadBoneOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 68));
+Vector3 vHipOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 2));
+Vector3 vNeckOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 67));
+Vector3 vUpperArmLeftOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 9));
+Vector3 vUpperArmRightOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 38));
+Vector3 vLeftHandOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 10));
+Vector3 vRightHandOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 39));
+Vector3 vLeftHandOut1 = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 11));
+Vector3 vRightHandOut1 = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 40));
+Vector3 vRightThighOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 76));
+Vector3 vLeftThighOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 69));
+Vector3 vRightCalfOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 77));
+Vector3 vLeftCalfOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 70));
+Vector3 vLeftFootOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 73));
+Vector3 vRightFootOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 80));
